f5803a837ae809b03f8fa295ca8aa3eef5b94e40
Brian Crowe <brian.j.crowe@protonmail.com>
2025-07-25 18:18:00 -0400

    change function to arrow function

--- js/challenge.js
+++ js/challenge.js
@@ -1,13 +1,13 @@
-function photoName() {
+const photoName = () => {
   //prompt person to enter name
-
-  var name = prompt('Please enter your name')
+  let name = prompt('Please enter your name')
 
   //remove additional whitespaces
 
   //replace the space between names with underscore
 
   //change name to all lowercase
-  alert('Your photo name is ' + name + '.jpg')
+  alert(`Your photo name is ${name}.jpg`)
 }
+
 photoName()
