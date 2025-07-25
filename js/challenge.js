0466646747aca47dbdcd29383fdcdb779b88f9da
Brian Crowe <brian.j.crowe@protonmail.com>
2025-07-25 18:40:02 -0400

    string methods use on prompt

--- js/challenge.js
+++ js/challenge.js
@@ -3,10 +3,14 @@ const photoName = () => {
   let name = prompt('Please enter your name')
 
   //remove additional whitespaces
+  name = name.trim()
 
   //replace the space between names with underscore
+  name = name.split(' ').join('_')
 
   //change name to all lowercase
+  name = name.toLowerCase()
+
   alert(`Your photo name is ${name}.jpg`)
 }
 
